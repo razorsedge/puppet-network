097037b2f5a67e5b27aa612aaa1dbca487d75cb8
Michael Arnold <github@razorsedge.org>
2012-11-22 11:54:57 -0700

    Add context 'singular parameters'.
    Added to network_route_spec.rb.

--- spec/defines/network_route_spec.rb
+++ spec/defines/network_route_spec.rb
@@ -4,7 +4,31 @@ require 'spec_helper'
 
 describe 'network::route', :type => 'define' do
 
-  context 'default parameters' do
+  context 'singular parameters' do
+    let(:title) { 'eth1' }
+    let :params do {
+      :address => '192.168.2.1',
+      :netmask => '255.255.255.1',
+      :gateway => '192.168.1.2',
+    }
+    end
+    it { should contain_file('route-eth1').with(
+      :ensure => 'present',
+      :mode   => '0644',
+      :owner  => 'root',
+      :group  => 'root',
+      :path   => '/etc/sysconfig/network-scripts/route-eth1'
+    )}
+    it 'should contain File[route-eth1] with contents "ADDRESS0=192.168.2.1\nNETMASK0=255.255.255.1\nGATEWAY0=192.168.1.2"' do
+      verify_contents(subject, 'route-eth1', [
+        'ADDRESS0=192.168.2.1',
+        'NETMASK0=255.255.255.1',
+        'GATEWAY0=192.168.1.2',
+      ])
+    end
+  end
+
+  context 'array parameters' do
     let(:title) { 'eth2' }
     let :params do {
       :address => [ '192.168.2.0', '10.0.0.0', ],
@@ -19,7 +43,7 @@ describe 'network::route', :type => 'define' do
       :group  => 'root',
       :path   => '/etc/sysconfig/network-scripts/route-eth2'
     )}
-    it 'should contain File[route-eth2] with contents "ADDRESS0=192.168.2.0\nADDRESS1=10.0.0.0"' do
+    it 'should contain File[route-eth2] with contents "ADDRESS0=192.168.2.0\nADDRESS1=10.0.0.0\nNETMASK0=255.255.255.0\nNETMASK1=255.0.0.0\nGATEWAY0=192.168.1.1\nGATEWAY1=10.0.0.1"' do
       verify_contents(subject, 'route-eth2', [
         'ADDRESS0=192.168.2.0',
         'ADDRESS1=10.0.0.0',
