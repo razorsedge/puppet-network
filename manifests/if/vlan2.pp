b3b461ea0492512b709548e1e71f9bf25130a82c
Neil Cornfoot <neilc@Neils-MacBook-Pro.local>
2013-08-19 23:38:25 +0100

    Vlan module

---
+++ manifests/if/vlan2.pp
@@ -0,0 +1,82 @@
+# == Definition: network::if::vlan2
+#
+# Creates a normal interface with static IP address.
+#
+# === Parameters:
+#
+#   $ensure       - required - up|down
+#   $ipaddress    - required
+#   $netmask      - required
+#   $gateway      - optional
+#   $macaddress   - optional - defaults to macaddress_$title
+#   $userctl      - optional - defaults to false
+#   $mtu          - optional
+#   $ethtool_opts - optional
+#   $peerdns      - optional
+#   $dns1         - optional
+#   $dns2         - optional
+#   $domain       - optional
+#
+# === Actions:
+#
+# Deploys the file /etc/sysconfig/network-scripts/ifcfg-$name.
+#
+# === Sample Usage:
+#
+#   network::if::static { 'eth0':
+#     ensure     => 'up',
+#     ipaddress  => '10.21.30.248',
+#     netmask    => '255.255.255.128',
+#     macaddress => $::macaddress_eth0,
+#     domain     => 'is.domain.com domain.com',
+#   }
+#
+# === Authors:
+#
+# Mike Arnold <mike@razorsedge.org>
+#
+# === Copyright:
+#
+# Copyright (C) 2011 Mike Arnold, unless otherwise noted.
+#
+define network::if::vlan2 (
+  $ensure,
+  $ipaddress,
+  $netmask,
+  $gateway = '',
+  $userctl = false,
+  $mtu = '',
+  $ethtool_opts = '',
+  $peerdns = false,
+  $dns1 = '',
+  $dns2 = '',
+  $domain = '',
+  $vlanis = ''
+) {
+  # Validate our data
+  if ! is_ip_address($ipaddress) { fail("${ipaddress} is not an IP address.") }
+
+  if ! is_mac_address($macaddress) {
+    $macaddy = getvar("::macaddress_${title}")
+  } else {
+    $macaddy = $macaddress
+  }
+  # Validate booleans
+  validate_bool($userctl)
+
+  network_if_base { $title:
+    ensure       => $ensure,
+    ipaddress    => $ipaddress,
+    netmask      => $netmask,
+    gateway      => $gateway,
+    macaddress   => $macaddy,
+    bootproto    => 'static',
+    userctl      => $userctl,
+    mtu          => $mtu,
+    ethtool_opts => $ethtool_opts,
+    dns1         => $dns1,
+    dns2         => $dns2,
+    domain       => $domain,
+    vlanis       => $vlanis,
+  }
+} # define network::if::vlan2
