2c564397ca1dea83adca8ddcc0130d07429b9273
Robert Frank <robert.frank@manchester.ac.uk>
2015-07-06 11:34:05 +0100

    added tests for ipv6secondaries

--- spec/defines/network_if_static_spec.rb
+++ spec/defines/network_if_static_spec.rb
@@ -13,24 +13,55 @@ describe 'network::if::static', :type => 'define' do
     }
     end
     it 'should fail' do
-      expect {should contain_file('ifcfg-eth77')}.to raise_error(Puppet::Error, /notAnIP is not an IP address./)
+      expect {should contain_file('ifcfg-eth77')}.to raise_error(Puppet::Error, /notAnIP is not an IP address\./)
     end
   end
 
   context 'incorrect value: ipv6address' do
     let(:title) { 'eth77' }
     let :params do {
-      :ensure    => 'up',
-      :ipaddress => '1.2.3.4',
-      :netmask   => '255.255.255.0',
+      :ensure      => 'up',
+      :ipaddress   => '1.2.3.4',
+      :netmask     => '255.255.255.0',
       :ipv6address => 'notAnIP',
     }
     end
     it 'should fail' do
-      expect {should contain_file('ifcfg-eth77')}.to raise_error(Puppet::Error, /notAnIP is not an IPv6 address./)
+      expect {should contain_file('ifcfg-eth77')}.to raise_error(Puppet::Error, /notAnIP is not an IPv6 address\./)
     end
   end
 
+  context 'incorrect hash value: ipv6address' do
+    let(:title) { 'eth77' }
+    let :params do {
+      :ensure      => 'up',
+      :ipaddress   => '1.2.3.4',
+      :netmask     => '255.255.255.0',
+      :ipv6address => { 'notAn' => 'IP' },
+    }
+    end
+    it 'should fail' do
+      expect {should contain_file('ifcfg-eth77')}.to raise_error(Puppet::Error, /{"notAn"=>"IP"} is not an IPv6 address\./)
+    end
+  end
+
+  context 'incorrect value: ipv6address in array' do
+    let(:title) { 'eth77' }
+    let :params do {
+      :ensure      => 'up',
+      :ipaddress   => '1.2.3.4',
+      :netmask     => '255.255.255.0',
+      :ipv6address => [
+        '123:4567:89ab:cdef:123:4567:89ab:cdee',
+        'notAnIP',
+        '123:4567:89ab:cdef:123:4567:89ab:cdef',
+      ]
+    }
+    end
+    it 'should fail' do
+      expect {should contain_file('ifcfg-eth77')}.to raise_error(Puppet::Error, /notAnIP is not an IP\(v6\) address\./)
+    end
+  end
 
   context 'required parameters' do
     let(:title) { 'eth1' }
@@ -173,4 +204,94 @@ describe 'network::if::static', :type => 'define' do
     it { should contain_service('network') }
   end
 
+  context 'optional parameters - single ipv6address in array' do
+    let(:title) { 'eth1' }
+    let :params do {
+      :ensure      => 'up',
+      :ipaddress   => '1.2.3.4',
+      :netmask     => '255.255.255.0',
+      :ipv6init    => true,
+      :ipv6address => [
+        '123:4567:89ab:cdef:123:4567:89ab:cdee',
+      ],
+    }
+    end
+    let :facts do {
+      :osfamily        => 'RedHat',
+      :macaddress_eth1 => 'fe:fe:fe:aa:aa:aa',
+    }
+    end
+    it { should contain_file('ifcfg-eth1').with(
+      :ensure => 'present',
+      :mode   => '0644',
+      :owner  => 'root',
+      :group  => 'root',
+      :path   => '/etc/sysconfig/network-scripts/ifcfg-eth1',
+      :notify => 'Service[network]'
+    )}
+    it 'should contain File[ifcfg-eth1] with required contents' do
+      verify_contents(catalogue, 'ifcfg-eth1', [
+        'DEVICE=eth1',
+        'BOOTPROTO=none',
+        'HWADDR=fe:fe:fe:aa:aa:aa',
+        'ONBOOT=yes',
+        'HOTPLUG=yes',
+        'TYPE=Ethernet',
+        'IPADDR=1.2.3.4',
+        'NETMASK=255.255.255.0',
+        'PEERDNS=no',
+        'IPV6INIT=yes',
+        'IPV6ADDR=123:4567:89ab:cdef:123:4567:89ab:cdee',
+        'NM_CONTROLLED=no',
+      ])
+    end
+    it { should contain_service('network') }
+  end
+
+  context 'optional parameters - multiple ipv6addresses' do
+    let(:title) { 'eth1' }
+    let :params do {
+      :ensure      => 'up',
+      :ipaddress   => '1.2.3.4',
+      :netmask     => '255.255.255.0',
+      :ipv6init    => true,
+      :ipv6address => [
+        '123:4567:89ab:cdef:123:4567:89ab:cded',
+        '123:4567:89ab:cdef:123:4567:89ab:cdee',
+        '123:4567:89ab:cdef:123:4567:89ab:cdef',
+      ],
+    }
+    end
+    let :facts do {
+      :osfamily        => 'RedHat',
+      :macaddress_eth1 => 'fe:fe:fe:aa:aa:aa',
+    }
+    end
+    it { should contain_file('ifcfg-eth1').with(
+      :ensure => 'present',
+      :mode   => '0644',
+      :owner  => 'root',
+      :group  => 'root',
+      :path   => '/etc/sysconfig/network-scripts/ifcfg-eth1',
+      :notify => 'Service[network]'
+    )}
+    it 'should contain File[ifcfg-eth1] with required contents' do
+      verify_contents(catalogue, 'ifcfg-eth1', [
+        'DEVICE=eth1',
+        'BOOTPROTO=none',
+        'HWADDR=fe:fe:fe:aa:aa:aa',
+        'ONBOOT=yes',
+        'HOTPLUG=yes',
+        'TYPE=Ethernet',
+        'IPADDR=1.2.3.4',
+        'NETMASK=255.255.255.0',
+        'PEERDNS=no',
+        'IPV6INIT=yes',
+        'IPV6ADDR=123:4567:89ab:cdef:123:4567:89ab:cded',
+        'IPV6ADDR_SECONDARIES="123:4567:89ab:cdef:123:4567:89ab:cdee 123:4567:89ab:cdef:123:4567:89ab:cdef"',
+        'NM_CONTROLLED=no',
+      ])
+    end
+    it { should contain_service('network') }
+  end
 end
