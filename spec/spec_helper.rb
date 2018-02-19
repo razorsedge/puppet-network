020ef28dfecc48a902903d48d517a1595052fad1
Philippe Muller <philippe.muller@gmail.com>
2018-02-19 22:57:33 +0800

    Prevent rspec output truncation
    Without this, long lines are truncated, making Puppet 4 type matching
    errors unreadable.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,4 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+# Prevent output truncation (useful for debugging)
+RSpec::Support::ObjectFormatter.default_instance.max_formatted_output_length = 999
