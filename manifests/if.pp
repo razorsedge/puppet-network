12d3127349ad585937969205170ea9caf6bd529e
Philippe Muller <philippe.muller@gmail.com>
2018-02-19 18:03:57 +0800

    Use typing in network::if

--- manifests/if.pp
+++ manifests/if.pp
@@ -34,20 +34,17 @@
 # Copyright (C) 2017 Mike Arnold, unless otherwise noted.
 #
 define network::if (
-  $ensure,
-  $manage_hwaddr = true,
-  $macaddress = undef,
-  $userctl = false,
-  $mtu = undef,
-  $ethtool_opts = undef,
-  $scope = undef,
-  $flush = false,
-  $zone = undef,
-  $restart = true,
+  Enum['up', 'down'] $ensure,
+  Boolean $manage_hwaddr = true,
+  Optional[Stdlib::MAC] $macaddress = undef,
+  Boolean $userctl = false,
+  Variant[Integer, String, Undef] $mtu = undef,
+  Optional[String] $ethtool_opts = undef,
+  Optional[String] $scope = undef,
+  Boolean $flush = false,
+  Optional[String] $zone = undef,
+  Boolean $restart = true,
 ) {
-  # Validate our regular expressions
-  $states = [ '^up$', '^down$' ]
-  validate_re($ensure, $states, '$ensure must be either "up" or "down".')
 
   if ! is_mac_address($macaddress) {
     # Strip off any tailing VLAN (ie eth5.90 -> eth5).
@@ -56,12 +53,6 @@ define network::if (
   } else {
     $macaddy = $macaddress
   }
-
-  # Validate booleans
-  validate_bool($userctl)
-  validate_bool($manage_hwaddr)
-  validate_bool($flush)
-  validate_bool($restart)
 
   network_if_base { $title:
     ensure        => $ensure,
