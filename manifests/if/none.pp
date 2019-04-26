6efb894e2ce12aa1da3f3346d09b87d914a64119
m4rx0 <m@footek.ch>
2019-04-26 17:06:54 +0200

    Add and fix data types in network::if::none

--- manifests/if/none.pp
+++ manifests/if/none.pp
@@ -30,19 +30,19 @@
 # Copyright (C) 2011 Mike Arnold, unless otherwise noted.
 #
 define network::if::none (
-  $ensure,
-  $macaddress = '',
-  $userctl = false,
-  $mtu = '',
-  $ethtool_opts = '',
-  $linkdelay = ''
+  Enum['up', 'down'] $ensure,
+  Optional[Stdlib::MAC] $macaddress = undef,
+  Boolean $userctl = false,
+  Optional[String] $mtu = undef,
+  Optional[String] $ethtool_opts = undef,
+  Optional[String] $linkdelay = undef
 ) {
   if ! is_mac_address($macaddress) and ($macaddress != 'unmanaged') {
     # Strip off any tailing VLAN (ie eth5.90 -> eth5).
     $title_clean = regsubst($title,'^(\w+)\.\d+$','\1')
     $macaddy = getvar("::macaddress_${title_clean}")
   } elsif $macaddress == 'unmanaged' {
-    $macaddy = ''
+    $macaddy = undef
   } else {
     $macaddy = $macaddress
   }
